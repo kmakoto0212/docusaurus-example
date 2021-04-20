0c5be7d0ea3cfa8971001e0641373d1767e096dd
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2021-04-20 17:31:18 +0900

    fix edit urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,13 +86,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/kmakoto0212/docusaurus-example/edit/main/docs/',
+            'https://github.com/kmakoto0212/docusaurus-example/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/kmakoto0212/docusaurus-example/edit/main/blog/',
+            'https://github.com/kmakoto0212/docusaurus-example/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
